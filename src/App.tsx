2cf46493977287ed8f13454a0a09322326519275
Ankur Sharma <148741048+Shsrma@users.noreply.github.com>
2026-02-07 18:41:08 +0530

    V44

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import BookConsultation from "./pages/BookConsultation";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Experience from "./pages/Experience";
+import Expertise from "./pages/Expertise";
+import Contact from "./pages/Contact";
+import BookConsultation from "./pages/BookConsultation";
 
 function App() {
   return (
@@ -13,6 +17,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/expertise" element={<Expertise />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/book" element={<BookConsultation />} />
       </Routes>
